aa563d1dd5b953b769acd1d093bf7c78a74b0d2c
sswtk <sswss5@aliyun.com>
2022-08-15 07:51:54 +0800

    feat: add interface practice

--- queue/entry/main.go
+++ queue/entry/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"go-practice-codehub/queue"
+)
 
+func main() {
+	q := queue.Queue{1}
+
+	q.Push(2)
+	q.Push(3)
+	fmt.Println(q.Pop())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+	q.Push("abc")
+	fmt.Println(q.Pop())
+	q.Push("ttt")
+	fmt.Println(q.Pop())
 }
---
+++ retriever/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"go-practice-codehub/retriever/mock"
+	"go-practice-codehub/retriever/real"
+	"time"
+)
+
+type Retriever interface {
+	Get(url string) string
+}
+
+type Poster interface {
+	Post(url string,
+		form map[string]string) string
+}
+
+const url = "http://www.imooc.com"
+
+func download(r Retriever) string {
+	return r.Get(url)
+}
+
+func post(poster Poster) string {
+	return poster.Post(url,
+		map[string]string{
+			"name":   "ssw",
+			"course": "golang",
+		})
+}
+
+type RetrieverPoster interface {
+	Retriever
+	Poster
+}
+
+func session(s RetrieverPoster) string {
+	s.Post(url, map[string]string{
+		"contents": "another faked imooc.com",
+	})
+	return s.Get(url)
+}
+
+func main() {
+	var r Retriever
+	retriever := mock.Retriever{"this is fake imooc.com"}
+	r = &retriever
+	inspect(r)
+
+	r = &real.Retriever{
+		UserAgent: "Mozilla/5.0",
+		TimeOut:   time.Minute,
+	}
+	inspect(r)
+
+	// Type assertion
+	realRetriever := r.(*real.Retriever)
+	fmt.Println(realRetriever.TimeOut)
+
+	fmt.Println("Try a session")
+	fmt.Println(session(&retriever))
+}
+
+func inspect(r Retriever) {
+	fmt.Printf("%T %v\n", r, r)
+	switch v := r.(type) {
+	case *mock.Retriever:
+		fmt.Println("Contents: ", v.Contents)
+	case *real.Retriever:
+		fmt.Println("UserAgent: ", v.UserAgent)
+	}
+}
